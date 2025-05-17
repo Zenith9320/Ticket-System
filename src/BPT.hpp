f0abd28d4eec774c729412144129b50bd995557e
Zenith9320 <zhangentong@sjtu.edu.cn>
2025-05-17 17:17:24 +0800

    change sizez

--- src/BPT.hpp
+++ src/BPT.hpp
@@ -15,7 +15,7 @@ using std::string;
 using std::fstream;
 using std::ios;
 
-const int SIZE = 120;
+const int SIZE = 88;
 const int STR_LEN = 100;
 
 /********************************************************************/
